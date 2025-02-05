6721fce969e7cdf20cdfa9a381baf61214fe5b36
DoniLite <yaomessan13@outlook.com>
2025-02-05 12:46:12 +0000

    initialization

---
+++ blockchain/block.go
@@ -0,0 +1,51 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          *Data
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+type Data struct {
+	Value []byte
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data.Value, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), &Data{[]byte(data)}, prevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
+
+type Blockchain struct {
+	Blocks []*Block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.Blocks[len(bc.Blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.Blocks = append(bc.Blocks, newBlock)
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Oroscop/Noxt
+
+go 1.23.5
